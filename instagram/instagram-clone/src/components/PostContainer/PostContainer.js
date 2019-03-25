d53212af9374d26fa2432af0e2c1dcc5d9289b11
NightlyD3V <TylerSpaulding95@gmail.com>
2019-03-25 20:45:34 +0100

    Added components folder structure

---
+++ instagram/instagram-clone/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PostContainer(props) {
+    return (
+        <div>
+            <h1>Post Container</h1>
+        </div>
+    )
+}
+
+export default PostContainer;
