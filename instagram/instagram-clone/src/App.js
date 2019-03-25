72dd08f8a27f45ca50ee172d6eeb9fe8408a69b6
NightlyD3V <TylerSpaulding95@gmail.com>
2019-03-25 21:18:53 +0100

    Added new dummyData and starting iteration

--- instagram/instagram-clone/src/App.js
+++ instagram/instagram-clone/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import DummyData from './dummy-data';
 import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+import CommentContainer from './components/CommentSection/CommentSection';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: DummyData,
+    }
+  }
   render() {
     return (
       <div className="App">
         <SearchBar/>
+        <PostContainer data={this.state.data}/>
+        <CommentContainer />
       </div>
     );
   }
--- instagram/instagram-clone/src/components/PostContainer/PostContainer.js
+++ instagram/instagram-clone/src/components/PostContainer/PostContainer.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 function PostContainer(props) {
+    console.log(props);
     return (
         <div>
             <h1>Post Container</h1>
+            {props.data.map((user) => (
+                <div key={user.id}>
+                    <h1>{user.username}</h1>
+                    <img src={user.imageUrl}/>
+                </div>
+            ))}
         </div>
     )
 }
